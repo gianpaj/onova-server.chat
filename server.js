4457338d0e1b52fbd89e229daa51a7ff4e507e70
Gianfranco Palumbo <gianpa@gmail.com>
2018-04-19 17:17:33 +0300

    change default port 8142

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import Chatkit from 'pusher-chatkit-server';
 const config = require('./config');
 
 const app = express();
-const port = process.env.PORT | 3030;
+const port = process.env.PORT | 8142;
 const configJSON = require('./config.json');
 const chatkit = new Chatkit({
   instanceLocator: configJSON.chatkit.instanceLocator,
