c34a2c39f691e8ef4935f857d79bccc753a4dbbb
Gianfranco Palumbo <gianpa@gmail.com>
2018-04-19 16:20:01 +0300

    server.js is the main file

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import Pusher from 'pusher';
 import Chatkit from 'pusher-chatkit-server';
 // import logger from 'morgan';
 
@@ -8,14 +7,6 @@ const config = require('./config');
 
 const app = express();
 const port = process.env.PORT | 3030;
-const pusher = new Pusher({
-  appId: config.APP_ID,
-  key: config.APP_KEY,
-  secret: config.APP_SECRET,
-  cluster: config.CLUSTER,
-  // encrypted: true
-});
-
 const configJSON = require('./config.json');
 const chatkit = new Chatkit({
   instanceLocator: configJSON.chatkit.instanceLocator,
@@ -45,58 +36,64 @@ app.post(config.ENDPOINT, (req, res, next) => {
   // ...
   // ...
   // ...
-  // Extract the socket id and channel name from the request body
-  //
-  const { socket_id, channel_name, grant_type } = req.body;
+  // TODO: validate JWT token
+
+  const { grant_type } = req.body;
 
   if (grant_type == 'client_credentials') {
-    const { token } = req.headers;
+    const { token, avatarURL, username } = req.headers;
 
-    // TODO: validate JWT token
     const { user_id } = req.query;
     const auth = chatkit.authenticate({
       userId: user_id,
       authPayload: req.body,
     });
     return chatkit
-      .createUser({
-        id: user_id,
-        name: 'Some name',
+      .updatePermissionsForGlobalRole({
+        roleName: 'default',
+        permissionsToAdd: [
+          'message:create',
+          'room:join',
+          // 'room:leave',
+          'room:get',
+          'room:create',
+          'room:messages:get',
+          'room:typing_indicator:create',
+          'presence:subscribe',
+          'user:get',
+          'user:rooms:get',
+          'cursors:read:get',
+          'cursors:read:set',
+          'file:create',
+          'file:get',
+          'room:delete',
+          'room:update',
+        ],
       })
       .then(() => {
-        console.log(token);
-        console.log(auth);
-        return res.json(auth);
-      })
-      .catch(e => {
-        if (e.error_type == 'services/chatkit/user_already_exists') {
-          return res.json(auth);
-        }
-        res.status(500).json({
-          ok: false,
-          error: e,
-        });
+        console.log('updatePermissionsForGlobalRole Success');
+        chatkit
+          .createUser({
+            id: user_id,
+            avatarURL,
+            name: username,
+          })
+          .then(() => {
+            console.log(token);
+            console.log(auth);
+            return res.json(auth);
+          })
+          .catch(e => {
+            if (e.error_type == 'services/chatkit/user_already_exists') {
+              return res.json(auth);
+            }
+            res.status(500).json({
+              ok: false,
+              error: e,
+            });
+          });
       });
   }
-
-  let auth;
-
-  if (/^presence-/.test(channel_name)) {
-    // If the request is for a presence channel include some data about the user
-    // in the call to authenticate
-    const timestamp = new Date().toISOString();
-    const presenceData = {
-      user_id: `user-${timestamp}`,
-      user_info: {
-        name: 'Pusherino',
-        twitter_id: '@pusher',
-      },
-    };
-    auth = pusher.authenticate(socket_id, channel_name, presenceData);
-  } else {
-    auth = pusher.authenticate(socket_id, channel_name);
-  }
-  res.send(auth);
 });
 
 app.listen(port, () => {
